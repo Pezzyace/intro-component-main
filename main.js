e23d499e146b0cb68f23f6f40c50b65dddb51bd9
Akaehomen peace <peacedavid049@gmail.com>
2023-09-28 15:25:24 +0100

    commit

--- main.js
+++ main.js
@@ -1,21 +1,54 @@
 const firstName = document.getElementById('first-name');
-const lastNAme = document.getElementById('last-name');
+const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const claimBtn = document.getElementById('claimBtn');
-const pattern = /^[A-Za-z0-9._]{3,}@[A_Za-z]{3,}[.]{1}[A-Za-z.]{2-6}$/
 
-claimBtn.addEventListener('click', () => {
-  function myfun(){
-    if (pattern.test(email)){
-      alert('Wrong Email');
-    }
+claimBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  var firstN = firstName.value.trim();
+  var lastN = lastName.value.trim();
+  var emailValue = email.value.trim();
+  var passwordValue = password.value.trim();
+
+  if(firstN === ''){
+    errorFunc(firstName, 'First Name cannot be empty');
+  }else{
+    successFunc(firstName);
+  }
+
+  if(lastN === ''){
+    errorFunc(firstName, 'Last Name cannot be empty');
+  }else{
+    successFunc(firstName);
+  }
+
+  if(emailValue === ''){
+    errorFunc(firstName, 'email Name cannot be empty');
+  }else{
+    successFunc(firstName);
+  }
+
+  if(passwordValue === ''){
+    errorFunc(firstName, 'passowrd cannot be empty');
+  }else{
+    successFunc(firstName);
   }
 })
 
-claimBtn.addEventListener('click', () => {
-  colsole.log('pizzy ace');
-})
+function errorFunc(req, message){
+  const formControl = req.parentElement;
+  const span = formControl.querySelector('span');
+  span.innerText = message;
+  req.className += 'error';
+  span.className += 'error-text';
+}
+
+function successFunc(req){
+  req.className == 'sucess';
+}
+
 
 
 

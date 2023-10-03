02c27aa45b74ddfeebf99d788d150b3e311b557b
Akaehomen peace <peacedavid049@gmail.com>
2023-10-03 17:49:54 +0100

    commit

--- main.js
+++ main.js
@@ -3,51 +3,63 @@ const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const claimBtn = document.getElementById('claimBtn');
+const error = document.getElementById("error");
+const firstNameErrorMessage = document.getElementById("firstNameErrorMessage");
+const lastNameErrorMessage = document.getElementById("lastNameErrorMessage");
+const emailErrorMessage = document.getElementById("emailErrorMessage");
+const passwordErrorMessage = document.getElementById("passwordErrorMessage");
+const firstNameError = document.getElementById("firstNameError");
+const lastNameError = document.getElementById("lastNameError");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
+const form = document.getElementById('form');
 
-claimBtn.addEventListener('click', e => {
+
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-
-  var firstN = firstName.value.trim();
-  var lastN = lastName.value.trim();
-  var emailValue = email.value.trim();
-  var passwordValue = password.value.trim();
-
-  if(firstN === ''){
-    errorFunc(firstName, 'First Name cannot be empty');
-  }else{
-    successFunc(firstName);
+   
+  const namePattern = /^[A-Za-z\s]+$/;
+  if (!namePattern.test(firstName.value.trim())) {
+      firstName.classList.add("err");
+      firstNameError.style.display = "inline-block";
+      firstNameErrorMessage.style.display = "block";
+  } else {
+      firstName.classList.remove("err");
+      firstNameError.style.display = "none";
+      firstNameErrorMessage.style.display = "none";
   }
 
-  if(lastN === ''){
-    errorFunc(firstName, 'Last Name cannot be empty');
-  }else{
-    successFunc(firstName);
+  if (!namePattern.test(lastName.value.trim())) {
+    lastName.classList.add("err");
+    lastNameError.style.display = "inline-block";
+    lastNameErrorMessage.style.display = "block";
+  } else {
+    lastName.classList.remove("err");
+    lastNameError.style.display = "none";
+    lastNameErrorMessage.style.display = "none";
   }
 
-  if(emailValue === ''){
-    errorFunc(firstName, 'email Name cannot be empty');
-  }else{
-    successFunc(firstName);
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!emailPattern.test(email.value.trim())) {
+      email.classList.add("err");
+      emailError.style.display = "inline-block";
+      emailErrorMessage.style.display = "block";
+  } else {
+      email.classList.remove("err");
+      emailError.style.display = "none";
+      emailErrorMessage.style.display = "none";
   }
 
-  if(passwordValue === ''){
-    errorFunc(firstName, 'passowrd cannot be empty');
-  }else{
-    successFunc(firstName);
+  if (password.value.length < 6) {
+      password.classList.add("err");
+      passwordError.style.display = "inline-block";
+      passwordErrorMessage.style.display = "block";
+  } else {
+      password.classList.remove("err");
+      passwordError.style.display = "none";
+      passwordErrorMessage.style.display = "none";
   }
-})
-
-function errorFunc(req, message){
-  const formControl = req.parentElement;
-  const span = formControl.querySelector('span');
-  span.innerText = message;
-  req.className += 'error';
-  span.className += 'error-text';
-}
-
-function successFunc(req){
-  req.className == 'sucess';
-}
+});
 
 
 
